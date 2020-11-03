e56f677a44a74f590cc19e47bc458454be5f44e3
allan-silveira <54253262+allan-silveira@users.noreply.github.com>
2020-11-03 10:59:58 -0300

    Correção de upload de imagens

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+mongoose.connect('mongodb://localhost/noderest', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
